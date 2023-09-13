a351f182d1713df8f083670f7081ac896d2977fd
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-09-13 13:31:59 -0300

    Adding tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/header";
 
 function App() {
   return (
-    <div>
+    <div className="container mx-auto mt-10">
       <Header />
       <Form />
       <PatientList />
