3d0fb55f1449f1e541a14c67f42b9a6438b35b15
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-09-14 13:34:03 -0300

    Styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 const Header = () => {
   return (
-    <div>header</div>
+    <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+      Seguimiento pacientes 
+      <span className="text-indigo-600">Veterinaria</span> 
+    </h1>
   )
 }
 
