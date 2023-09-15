734faf3911a0791945a8859307fd9bdac4d307b3
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-09-15 09:04:48 -0300

    Design patientList finished

---
+++ src/components/Patient.jsx
@@ -0,0 +1,23 @@
+const Patient = () => {
+  return (
+    <div className="mt-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Name: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Owner: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">High medical: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Symptoms: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+      </div>
+  )
+}
+
+export default Patient
