5aafabb8901f72202a0d94a9c6abe4b290e73586
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-09-18 13:15:56 -0300

    form state ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import PatientList from "./components/PatientList";
 import Header from "./components/header";
 
 function App() {
+  const [ patients, setPatients ] = useState([]);
+
   return (
     <div className="container mx-auto mt-10">
       <Header />
       <div className="mt-12 md:flex">
-        <Form />
+        <Form 
+          patients={patients}
+          setPatients={setPatients}
+        />
         <PatientList />
       </div>
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,55 @@
-const Form = () => {
+import { useState, useEffect } from 'react';
+
+const Form = ({ setPatients, patients }) => {
+  const [ name, setName ] = useState('');
+  const [ owner, setOwner ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ high, setHigh ] = useState('');
+  const [ symptoms, setSymptoms ] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if([name, owner, email, high, symptoms].includes('')){
+      setError(true);
+      return;
+    };
+
+    setError(false);
+
+    const patient = {
+      name,
+      owner, 
+      email, 
+      high, 
+      symptoms
+    };
+
+    setPatients([...patients, patient]);
+
+    setName('');
+    setOwner('');
+    setEmail('');
+    setHigh('');
+    setSymptoms('');
+  };
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Añade pacientes y {''}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" action="">
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 "
+      onSubmit= {handleSubmit}>
+        { error && 
+          (<div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
+            <p>All fields are required</p>
+          </div>)
+        }
         <div className="mb-5">
           <label htmlFor="petName" 
             className="block text-gray-700 uppercase font-bold">
@@ -18,6 +60,8 @@ const Form = () => {
             placeholder="Pet name"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="petName"
+            value={name}
+            onChange={(e)=>{setName(e.target.value)}}
           />       
         </div>
         <div className="mb-5">
@@ -30,6 +74,8 @@ const Form = () => {
             placeholder="Owner name"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="owner"
+            value={owner}
+            onChange={(e)=>{setOwner(e.target.value)}}
           />       
         </div>
         <div className="mb-5">
@@ -43,6 +89,8 @@ const Form = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="email"
             autoComplete="false"
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
           />       
         </div>
         <div className="mb-5">
@@ -54,6 +102,8 @@ const Form = () => {
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="highMedical"
+            value={high}
+            onChange={(e)=>{setHigh(e.target.value)}}
           />       
         </div>
         <div className="mb-5">
@@ -65,7 +115,9 @@ const Form = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
             name=""
             placeholder="Describe symptoms"
-            id="symptoms" 
+            id="symptoms"
+            value={symptoms}
+            onChange={(e)=>{setSymptoms(e.target.value)}} 
           />
         </div>
 
